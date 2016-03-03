7f80e70bc00bb51635091a56e4888f144fbeb06b
Quentin Varquet <quentin.varquet@gmail.com>
2016-03-03 23:23:40 +0100

    delete https

--- Debian/Wheezy/docker-install.sh
+++ Debian/Wheezy/docker-install.sh
@@ -4,7 +4,7 @@ apt-get --yes upgrade &&
 apt-get --yes update  && 
 apt-get install --yes apt-transport-https ca-certificates && 
 apt-key adv --keyserver hkpp80.pool.sks-keyservers.net80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && 
-echo deb https://apt.dockerproject.org/repo debian-wheezy main  > /etc/apt/sources.list.d/docker.list && 
+echo deb http://apt.dockerproject.org/repo debian-wheezy main  > /etc/apt/sources.list.d/docker.list && 
 apt-get --yes update  && 
 sudo apt-get --yes update && 
 sudo apt-get install --yes docker-engine  && 
