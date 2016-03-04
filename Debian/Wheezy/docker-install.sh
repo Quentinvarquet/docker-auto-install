833308d1731a145a321f238f670cd49b6c0a2016
Quentin Varquet <quentin.varquet@gmail.com>
2016-03-04 10:40:44 +0100

    some fix because i was tired yesterday...

--- Debian/Wheezy/docker-install.sh
+++ Debian/Wheezy/docker-install.sh
@@ -3,7 +3,7 @@ apt-get --yes install sudo &&
 apt-get --yes upgrade && 
 apt-get --yes update  && 
 apt-get install --yes apt-transport-https ca-certificates && 
-apt-key adv --keyserver hkpp80.pool.sks-keyservers.net80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && 
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && 
 echo deb http://apt.dockerproject.org/repo debian-wheezy main  > /etc/apt/sources.list.d/docker.list && 
 apt-get --yes update  && 
 sudo apt-get --yes update && 
