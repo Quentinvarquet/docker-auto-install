ac4903529700b6206c0e59d88908cdeca830d25a
Quentin Varquet <quentin.varquet@gmail.com>
2016-03-04 11:26:39 +0100

    add docker-compose line

--- Debian/Wheezy/docker-install.sh
+++ Debian/Wheezy/docker-install.sh
@@ -11,4 +11,5 @@ sudo apt-get install --yes docker-engine  &&
 sudo service docker start && 
 apt-get install --yes curl  && 
 curl -L https://github.com/docker/compose/releases/download/1.6.2/run.sh > /usr/local/bin/docker-compose &&
-chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose &&
+docker-compose
